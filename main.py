d5a8f9780e2302a7483a5ccb7b143966bf114860
alizarazot <alizarazot@ufpso.edu.co>
2024-11-14 17:01:59 -0500

    Format code with Black

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from matplotlib import pyplot as plot
 import glob
 import sys
 
+
 def main():
     debug = False
     if len(sys.argv) == 2 and sys.argv[1] == "debug":
@@ -26,9 +27,11 @@ def main():
     for image in glob.iglob("assets/car*.jpg"):
         get_plate(image, debug)
 
+
 def get_plate(img_path, debug=False):
     image = cv.imread(img_path)
-    if debug: show_image('Imagen original', image)
+    if debug:
+        show_image("Imagen original", image)
 
     """
     Paso 1: Simplificar imagen.
@@ -38,11 +41,11 @@ def get_plate(img_path, debug=False):
     image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     # Reducir ruido.
     image_gray = cv.bilateralFilter(image_gray, 11, 17, 17)
-    #show_image('Imagen en escala de grises', image_gray)
+    # show_image('Imagen en escala de grises', image_gray)
 
     # Extraer bordes de la imagen.
     image_edged = cv.Canny(image_gray, 30, 200)
-    #show_image('Bordes de la imagen', image_edged)
+    # show_image('Bordes de la imagen', image_edged)
 
     """
     Paso 2: Buscar la placa.
@@ -53,7 +56,7 @@ def get_plate(img_path, debug=False):
     contours = imutils.grab_contours(contours)
     # Elegir los 10 mejores contornos.
     contours = sorted(contours, key=cv.contourArea, reverse=True)
-    
+
     # Buscar el contorno que se parezca más a un rectángulo (4 lados).
     location = None
     for contour in contours:
@@ -61,33 +64,37 @@ def get_plate(img_path, debug=False):
         if len(approx) == 4:
             location = approx
             break
-    #print("Ubicación de la placa:", location)
+    # print("Ubicación de la placa:", location)
 
     # Remover todo excepto la placa en la imagen.
-    image_mask = np.zeros(image_gray.shape, np.uint8) # Imagen negra del mismo tamaño de la original.
+    image_mask = np.zeros(
+        image_gray.shape, np.uint8
+    )  # Imagen negra del mismo tamaño de la original.
     image_cleaned = cv.drawContours(image_mask, [location], 0, 255, -1)
     image_cleaned = cv.bitwise_and(image, image, mask=image_mask)
-    if debug: show_image('Solo la placa en la imagen', image_cleaned)
+    if debug:
+        show_image("Solo la placa en la imagen", image_cleaned)
 
     # Crear nueva imagen conteniendo solamente la placa.
-    x, y = np.where(image_mask == 255) # Coordenadas de las esquinas de la placa.
-    x1, y1 = np.min(x), np.min(y) # Coordenadas superior izquierda de la placa.
-    x2, y2 = np.max(x), np.max(y) # Coordenadas inferior derecha de la placa.
+    x, y = np.where(image_mask == 255)  # Coordenadas de las esquinas de la placa.
+    x1, y1 = np.min(x), np.min(y)  # Coordenadas superior izquierda de la placa.
+    x2, y2 = np.max(x), np.max(y)  # Coordenadas inferior derecha de la placa.
     # Recortar la imagen.
-    image_plate = image_gray[x1:x2+1, y1:y2+1]
-    #show_image('Placa', image_plate)
+    image_plate = image_gray[x1 : x2 + 1, y1 : y2 + 1]
+    # show_image('Placa', image_plate)
 
     """
     Paso 3: Usar OCR para extraer el texto de la placa.
     """
 
-    r = easyocr.Reader(['en'])
+    r = easyocr.Reader(["en"])
     plate = r.readtext(image_plate)
     print(plate)
     plate = plate[0][1]
 
     # Mostrar la imagen.
-    show_image(f'Placa: {plate}', image)
+    show_image(f"Placa: {plate}", image)
+
 
 def show_image(title, image, cvt_color=cv.COLOR_BGR2RGB):
     image = cv.cvtColor(image, cvt_color)
@@ -96,4 +103,6 @@ def show_image(title, image, cvt_color=cv.COLOR_BGR2RGB):
     plot.title(title)
     plot.show()
 
-if __name__ == '__main__': main()
+
+if __name__ == "__main__":
+    main()
